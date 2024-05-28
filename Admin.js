a901f7164e50d920d494315bcd85d98bc9a52aa8
JulianaAmoasei <late.parrot@gmail.com>
2024-05-28 10:30:46 -0300

    video: adicionando métodos

--- Admin.js
+++ Admin.js
@@ -4,8 +4,12 @@ class Admin extends User {
   constructor(nome, email, nascimento, role = "admin", ativo = "true") {
     super(nome, email, nascimento, role, ativo)
   }
+
+  criarCurso(nomeCurso, qtdVagas) {
+    return `curso ${nomeCurso} criado com ${qtdVagas} vagas.`
+  }
 }
 
 const novoAdmin = new Admin("Rodrigo", "r@r.com", "2024-01-01")
 console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.criarCurso('JavaScript', 20));

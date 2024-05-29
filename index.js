523d86fb5622c9f47e9b877ff78a4a4cae1eea2f
JulianaAmoasei <late.parrot@gmail.com>
2024-05-29 09:58:33 -0300

    video: polimorfismo

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ import Docente from "./Docente.js"
 
 const novoUser = new User("Juliana", "j@j.com", "2024-01-01")
 console.log(novoUser.exibirInfos());
-novoUser.nome = "Juliana"
-console.log(novoUser.nome);
 
-// novoUser.#nome = "Márcia"
-// console.log(novoUser.exibirInfos());
-// console.log(novoUser.#nome);
+const novoAdmin = new Admin("Rodrigo", "r@r.com", "2024-01-01")
+console.log(novoAdmin.exibirInfos());
 
-// const novoAdmin = new Admin("Rodrigo", "r@r.com", "2024-01-01")
-// console.log(novoAdmin.nome);
-// console.log(novoAdmin.exibeNome());
